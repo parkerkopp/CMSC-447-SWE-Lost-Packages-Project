62584eadb918f63d1da1e171bb6929e5df3c8bce
Andrew Velasquez <andrewvel35@gmail.com>
2025-09-16 14:02:44 -0400

    Added umbc logo to assets folder and implemented to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    <img
+      src="./assets/UMBC-primary-logo.png"
+      alt="UMBC logo"
+      width="400"
+      height="100"
+      class="logo"
+    />
     <nav class="nav-bar">
       <router-link to="/" class="link">Home</router-link>
       <router-link to="/profile" class="link">Profile</router-link>
@@ -11,3 +18,10 @@
     <router-view />
   </div>
 </template>
+
+<style scoped>
+.logo {
+  display: flex;
+  margin: auto;
+}
+</style>
