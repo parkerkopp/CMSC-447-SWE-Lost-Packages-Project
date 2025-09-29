bc763aa79c1078192bb77413cb5eef5eda2c312f
Andrew Velasquez <andrewvel35@gmail.com>
2025-09-29 10:47:10 -0400

    Changed styling for reports page to make it more simplistic

---
+++ lost-packages-app/app/app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <header>
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/reports">Reports</NuxtLink>
+      <NuxtLink to="/lost_packages">Lost Packages</NuxtLink>
+      <NuxtLink to="/profile">Profile</NuxtLink>
+    </header>
+    <main>
+      <NuxtPage />
+    </main>
+  </div>
+</template>
