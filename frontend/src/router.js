6631e40640a58e6d7d3aa52865615955d712d181
Andrew Velasquez <andrewvel35@gmail.com>
2025-10-22 09:49:01 -0400

    Added a SignIn and SignUp page. Also added routes to those pages.

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -5,11 +5,15 @@ import LostPackagesPage from "./pages/LostPackagesPage.vue";
 import ProfilePage from "./pages/ProfilePage.vue";
 import ReportsPage from "./pages/ReportsPage.vue";
 import HomePage from "./pages/HomePage.vue";
+import SignIn from "./pages/SignIn.vue";
+import SignUp from "./pages/SignUp.vue";
 
 const routes = [
   { path: "/", component: HomePage },
   { path: "/lost-packages", component: LostPackagesPage },
   { path: "/profile", component: ProfilePage },
+  { path: "/signin", component: SignIn },
+  { path: "/signup", component: SignUp },
   { path: "/reports", component: ReportsPage },
 ];
 
