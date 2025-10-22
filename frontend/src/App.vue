2d4fab7515666ae2c7c613f11094e533acc771be
Andrew Velasquez <andrewvel35@gmail.com>
2025-10-22 10:06:03 -0400

    Added Title and removed some assets that were no longer needed. Added another asset icon.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <nav class="nav-bar">
+      <div class="title-container">
+        <img src="./assets/package.png" alt="package" class="icon" />
+        <h1 class="title">Lost Package Services</h1>
+      </div>
       <router-link to="/" class="link">Home</router-link>
       <router-link to="/profile" class="link">Profile</router-link>
       <router-link to="/reports" class="link">Reports</router-link>
