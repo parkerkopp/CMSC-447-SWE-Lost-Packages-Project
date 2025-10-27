d6d05485358af3abc1f093ed2f480d668d9dc311
broshio <lionmaster03@gmail.com>
2025-10-27 14:28:50 -0400

    Added skeleton for the ProfilePage

--- frontend/src/pages/ProfilePage.vue
+++ frontend/src/pages/ProfilePage.vue
@@ -1,5 +1,32 @@
 <template>
-  <div>Profile Packages Page</div>
+  <div>
+<header>
+    <h1>Employee Profile</h1>
+  </header>
+  <div><label for="worker_id">Worker ID</label>
+    <input id="worker_id" type="text" value="MM01234" readonly></input>
+  </div>
+
+  <div><label for="first_name">First Name</label>
+    <input id="first_name" type="text" value="Joe" readonly></input>
+  </div>
+
+  <div><label for="last_name">Last Name</label>
+    <input id="last_name" type="text" value="Shmo" readonly></input>
+  </div>
+
+  <div><label for="worker_phone">Phone Number</label>
+  <input id="worker_phone" type="text" value="240-112-2334" readonly></input>
+  </div>
+  
+  <div><label for="worker_email">Email Address</label>
+  <input id="worker_email" type="text" value="mm01234@umbc.edu" readonly></input>
+  </div>
+  
+  <div><label for="position">Position</label>
+  <input id="position" type="text" value="Delivery" readonly></input>
+  </div>
+  </div>
 </template>
 
 <script setup></script>
